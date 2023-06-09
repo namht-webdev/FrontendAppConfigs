938362f9f30d8c1fdd8179a5d1b0da59a0146aec
namht-webdev <namitus.hcmus@gmail.com>
2023-06-09 15:22:12 +0700

    update package

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Learn React</header>
+    <div className="text-center">
+      <header className="text-red-600 font-bold text-2xl uppercase pt-12">
+        Welcome to my initial config for applications
+      </header>
     </div>
   );
 }
